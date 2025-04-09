15d640ab08cd430c0bd749431dae0576be48c3fc
WebForks <35718739+WebForks@users.noreply.github.com>
2025-04-09 08:30:32 -0700

    Update subscriptions.tsx

--- receipt-tracker/app/(sub-more)/subscriptions.tsx
+++ receipt-tracker/app/(sub-more)/subscriptions.tsx
@@ -5,8 +5,8 @@ import "~/global.css";
 
 export default function Subscriptions() {
   return (
-    <View className="flex-1 justify-center items-center bg-white dark:bg-gray-900">
-      <Text className="text-xl font-bold dark:text-white">Subscriptions</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-xl font-bold ">Subscriptions</Text>
     </View>
   );
 }
